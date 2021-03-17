4cc19ff82140bc4d7e30ec19fc9c7ab8aa3b6e6e
samira077 <78590337+samira077@users.noreply.github.com>
2021-03-17 17:36:51 +0600

    Add files via upload

---
+++ labtask3/registrationinfo.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2021 at 01:25 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrationinfo`
+--
+
+CREATE TABLE `registrationinfo` (
+  `Name` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `User_Name` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Confirm_Password` varchar(255) NOT NULL,
+  `Gender` varchar(255) NOT NULL,
+  `Date_OfBirth` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `registrationinfo`
+--
+
+INSERT INTO `registrationinfo` (`Name`, `Email`, `User_Name`, `Password`, `Confirm_Password`, `Gender`, `Date_OfBirth`) VALUES
+('Shamira Yesmin', 'samira13@gmail.com', 'samira077', 'shopnil7', 'shopnil7', 'Female', '10-01-1999'),
+('Shamira Yesmin', 'samira13@gmail.com', 'samira077', 'shopnil7', 'shopnil7', 'Female', '10-01-1999');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
